a9d7913a7bfa233edcb69dc583f2552a1f8d2f99
Abdulhakeem Ahmed <keemahmed99@gmail.com>
2025-11-10 08:56:38 +0100

    corrections

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,17 +1,5 @@
 let arg = ['abdul', 'hbtn', 'cool'];
 
 if (arg.length > 1  && arg.length <= 3){console.log('Arguments found')}
-
-
-else{console.log('no argument');}
-
-let rag = ['abdul'];
-
-if(rag.length == 1){console.log('Argument found');}
-
-else{console.log('no argument');}
-
-let gar = [];
-if(gar.length == '1'){console.log('Argument found');}
-
+else if(arg.length == 1){console.log('Argument found');}
 else{console.log('no argument');}
--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,9 +1,6 @@
 
 
 const myvaD = ['HBTN', 'Cool'];
-if (myvaD  !== '[]'){console.log(myvaD[0]);}
+if (myvaD  == [n]){console.log(myvaD[0]);}
 else {console.log('No argument');}
 
-const myva = [];
-if (myva  !== '[]'){console.log(myva[0]);}
-else {console.log('No argument');}
