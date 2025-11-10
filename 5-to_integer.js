fbdc0c9f9052a9b6e022acdb16e8d4b3bd2d56b8
Abdulhakeem Ahmed <keemahmed99@gmail.com>
2025-11-10 20:30:59 +0100

    fixed issue

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -1,4 +1,4 @@
-const score = Process.argv[2];
+const score = process.argv[2];
 const number = parseInt(score);
 if(!isNaN(number)){console.log(`My number: ${number}`);}
 else{console.log('Not a number');}
