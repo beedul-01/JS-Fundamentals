38f7d683d4152c9b2e8e013ccb7892ff586c3d71
Abdulhakeem Ahmed <keemahmed99@gmail.com>
2025-11-10 09:32:58 +0100

    more changes

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,6 +1,6 @@
 
 
-const myvaD = ['HBTN', 'Cool'];
-if (myvaD  == []){console.log(myvaD[0]);}
+const myvaD = 'HBTN Cool';
+if (myvaD  !== ''){console.log(myvaD[0]);}
 else {console.log('No argument');}
 
