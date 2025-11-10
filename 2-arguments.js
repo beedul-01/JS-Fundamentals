2bdf9a203407b5f0a4be2f632c4c1683e1684565
Abdulhakeem Ahmed <keemahmed99@gmail.com>
2025-11-10 09:43:44 +0100

    more changes

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,5 +1,8 @@
 let arg = ['abdul', 'hbtn', 'cool'];
-
-if (arg.length > 1  && arg.length == 3){console.log('Arguments found')}
-else if(arg.length == 1){console.log('Argument found');}
-else{console.log('no argument');}
+if(arg.length >  1 && arg.length ==3 ){
+    console.log('Arguments found');
+}
+else if(arg.length ==1){
+    console.log('Argument found');
+}
+else{console.log('No argument');}
--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,6 +1,8 @@
 
 
-const myvaD = 'HBTN Cool';
-if (myvaD  !== ''){console.log(myvaD[0]);}
+const myvaD = ['HBTN', 'Cool'];
+if (myvaD  == []){
+    console.log(myvaD[0]);
+}
 else {console.log('No argument');}
 
